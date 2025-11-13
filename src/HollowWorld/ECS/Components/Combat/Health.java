622b94452889bb77f7f49e3947d47d81cbe6d7d8
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2025-11-13 17:10:10 +0100

    +Health ECS

--- src/HollowWorld/ECS/Components/Combat/Health.java
+++ src/HollowWorld/ECS/Components/Combat/Health.java
@@ -1,6 +1,8 @@
 package HollowWorld.ECS.Components.Combat;
 
 import HollowWorld.ECS.Components.Component;
-
 public class Health extends Component {
+    private static final int MAX_HEALTH = 20;
+    private int currentHealth = MAX_HEALTH;
 }
+
