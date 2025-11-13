705d6b11db65e2c3ba2f021c36ef8e3ff0d8e79f
Luxxinator1 <lukas.koenig.regensburg@protonmail.com>
2025-11-13 17:38:33 +0100

    +Fix bug in health ECS

--- src/HollowWorld/ECS/Components/Combat/Health.java
+++ src/HollowWorld/ECS/Components/Combat/Health.java
@@ -2,7 +2,7 @@ package HollowWorld.ECS.Components.Combat;
 
 import HollowWorld.ECS.Components.Component;
 public class Health extends Component {
-    private static final int MAX_HEALTH = 20;
-    private int currentHealth = MAX_HEALTH;
+    public static final int MAX_HEALTH = 20;
+    public int currentHealth = MAX_HEALTH;
 }
 
