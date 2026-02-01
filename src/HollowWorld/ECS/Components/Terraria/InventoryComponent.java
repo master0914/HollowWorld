5292bf323de83254d8ee3c5b265e608227c0d1f1
Lorenz <ddf9kkddqv@privaterelay.appleid.com>
2026-02-01 21:52:00 +0100

    + Array für Inventar und aufsammeln Methode

--- src/HollowWorld/ECS/Components/Terraria/InventoryComponent.java
+++ src/HollowWorld/ECS/Components/Terraria/InventoryComponent.java
@@ -2,5 +2,19 @@ package HollowWorld.ECS.Components.Terraria;
 
 import HollowWorld.ECS.Components.Component;
 
+
+
+import HollowWorld.ECS.Components.Terraria.ItemType;
+
 public class InventoryComponent extends Component {
+    public ItemType[] inventory = new ItemType[3];
+    public int zaehler = 0;
+
+public void collectItem(ItemType item){
+    if (zaehler < inventory.length){
+        inventory[zaehler] = item;
+        zaehler ++;
+    }
 }
+}
+
