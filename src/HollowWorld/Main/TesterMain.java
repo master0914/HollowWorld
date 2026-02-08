b798ba3974b31ba1379c4a11c9093224081c4a93
berg4444 <berg4444@web.de>
2026-02-08 19:47:17 +0100

    + tester main leer wieder

--- src/HollowWorld/Main/TesterMain.java
+++ src/HollowWorld/Main/TesterMain.java
@@ -1,24 +1,9 @@
 package HollowWorld.Main;
 
-import Engine.Core.GameContainer;
-import HollowWorld.ECS.Components.Terraria.ItemType;
-import HollowWorld.ECS.GameObjects.GameObject;
-import HollowWorld.GameCode.EntityFactory;
-import HollowWorld.GameCode.MainGame;
-import HollowWorld.ECS.AbstractBoilerPlateGame;
 
 
-public class TesterMain {
+public class TesterMain{
     public static void main(String[] args) {
-        ItemType item = ItemType.STONE;
-        EntityFactory.makeItem(ItemType.DIRT,300,200);
-
-
-
-
-
-
     }
-
 
 }
