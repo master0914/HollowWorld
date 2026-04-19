468e57901e56b7ba48cf5779812f5e62034c035d
berg4444 <berg4444@web.de>
2026-04-19 18:17:02 +0200

    Blöcke brauchen unterschiedliche lange zum Abbauen

--- src/HollowWorld/ECS/GameSystems/MiningSystem.java
+++ src/HollowWorld/ECS/GameSystems/MiningSystem.java
@@ -4,50 +4,36 @@ import Engine.Core.GameContainer;
 import Engine.Math.IVector;
 import HollowWorld.ECS.Components.Player.PlayerInput;
 import HollowWorld.ECS.Components.Terraria.BlockType;
-import HollowWorld.ECS.Components.Terraria.ItemType;
 import HollowWorld.ECS.GameObjects.GameObject;
 import HollowWorld.GameCode.EventManager;
 import HollowWorld.GameCode.GameData;
 import HollowWorld.GameCode.WorldGeneration.WorldMap;
 
 
-import Engine.Core.GameContainer;
-import HollowWorld.ECS.Events.MousePressedEvent;
-import HollowWorld.ECS.Components.Player.PlayerInput;
-import HollowWorld.ECS.Components.Terraria.BlockType;
-import HollowWorld.ECS.Components.Terraria.ItemType;
-import HollowWorld.ECS.Events.DropItemBlock;
-import HollowWorld.ECS.Events.MousePressedEvent;
-import HollowWorld.ECS.GameObjects.GameObject;
-import HollowWorld.GameCode.EventManager;
-import HollowWorld.ECS.GameSystems.RenderSystem;
-import HollowWorld.GameCode.WorldGeneration.WorldMap;
-
-import javax.swing.plaf.basic.BasicTreeUI;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.Arrays;
-import java.util.List;
-
-
-import java.util.List;
 
 import HollowWorld.ECS.Events.DropItemBlock;
 
+import java.util.List;
+import java.util.concurrent.*;
 
 
 public class MiningSystem extends GameSystem {
 
+    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    boolean mining = false;
+    float current_block_hp = -1;
+    IVector miningBlockpos;
 
     @Override
     public void initialize() {
 
+
     }
 
     @Override
-    public void cleanup() {}
-
+    public void cleanup() {
+    }
 
 
     @Override
@@ -60,19 +46,41 @@ public class MiningSystem extends GameSystem {
 
 
 
-        // MousePressedEvent auswerten
-        if(input.isMouseLeftJustPressed()) {
-
+        if (input.isMouseLeft()){
             IVector blockpos = GameData.screenXYtoBlockXY(input.getMouseX(), input.getMouseY());
-            BlockType p =  worldMap.getBlock(blockpos.x, blockpos.y);
+            BlockType p = worldMap.getBlock(blockpos.x, blockpos.y);
 
-            if(p != BlockType.AIR) {
-                EventManager.addEvent(new DropItemBlock(p.dropItem, 1, input.getMouseX(), input.getMouseY()));
-                worldMap.removeBlock(blockpos.x, blockpos.y);
+            if(p != BlockType.AIR){
+
+
+                if(current_block_hp < 0){
+                    current_block_hp = p.hardness;
+
+                }
+
+
+
+                Runnable scheduleMineBlock = () -> mineBlock();
+
+                if(!mining){
+                    if(current_block_hp <= 0){
+                        current_block_hp = -1;
+                        EventManager.addEvent(new DropItemBlock(p.dropItem, 1, input.getMouseX(), input.getMouseY()));
+                        worldMap.removeBlock(blockpos.x, blockpos.y);
+
+                    }else{
+                        mining = true;
+                        ScheduledFuture<?> schedFuture = scheduler.schedule(scheduleMineBlock,1,TimeUnit.SECONDS);
+                    }
+                }
             }
         }
+    }
 
-
+    public void mineBlock(){
+        current_block_hp -= 0.5f;
+        mining = false;
+        System.out.println(current_block_hp);
     }
 }
 
